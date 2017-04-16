761c0b5eb1054d97c34f0ae059405122fa550a55
mraduldubey <mraduldubeymd19@gmail.com>
2017-04-16 21:31:44 +0530

    Day 9

---
+++ Maths/l42.py
@@ -0,0 +1,5 @@
+a = int(raw_input().strip())
+b = int(raw_input().strip())
+m = int(raw_input().strip())
+print a**b
+print pow(a,b,m)
---
+++ Maths/l43.py
@@ -0,0 +1,6 @@
+a = int(raw_input().strip())
+b = int(raw_input().strip())
+c = int(raw_input().strip())
+d = int(raw_input().strip())
+
+print a**b + c**d
